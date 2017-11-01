4663feb1a673268a899ac5992ed6646a2081e09a
KarinaDavtyan <k.davtyan27@gmail.com>
2017-11-01 16:34:21 +0100

    fix: getting reddit data via api middleware

--- store.js
+++ store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 
 import logger from './middlewares/logger';
+import api from './middlewares/api';
 import reducers from './reducers';
 
-const store = createStore(reducers, applyMiddleware(logger));
+const store = createStore(reducers, applyMiddleware(logger, api));
 
 export default store;
