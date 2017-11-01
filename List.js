e19d4af5dc76448cbc1fbc48be71c0473210abf4
KarinaDavtyan <k.davtyan27@gmail.com>
2017-11-01 20:33:11 +0100

    feat: show flat list with reddit tittle

--- List.js
+++ List.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
 
 import {connect} from 'react-redux';
 
 import { getReddits } from './actions';
+import ListItem from './ListItem';
 
 class List extends React.Component {
-
 
   render() {
     return (
       <View style={styles.container}>
         <Button
-          onPress={() => this.props.getReddits()}
+          onPress={() => {
+            this.props.getReddits()
+            if (this.props.reddits.length >0) {
+              console.log('here',this.props);
+            } else {
+              console.log('its happening', this.props.reddits);
+            }
+          }}
           title="Refresh the state"
           color="blue"
+        />
+        <FlatList
+          data={this.props.reddits}
+          renderItem={({item}) => {
+            return (
+              <ListItem reddit={item} />
+            )
+          }}
+          keyExtractor={item => item.data.id}
         />
       </View>
     );
